ff3f1d7a3efb4b7ac8c8de79f48afc06aba8552d
Julian Enrique <julian.buitrago3003@gmail.com>
2024-10-29 04:36:47 -0500

    add env variables application.properties

--- src/main/java/org/usco/parkingsystemapi/controller/TestController.java
+++ src/main/java/org/usco/parkingsystemapi/controller/TestController.java
@@ -1,4 +1,13 @@
 package org.usco.parkingsystemapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
